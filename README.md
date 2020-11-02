04b41542e39d5870259f63ee3c4e5ced31f13ed2
Ali Uneri <ali.uneri@jhu.edu>
2020-11-01 21:35:25 -0500

    Unit testing

--- README.md
+++ README.md
@@ -5,3 +5,12 @@
 ```shell
 pip install git+https://git.lcsr.jhu.edu/auneri1/pyMetaIO.git
 ```
+
+## Testing
+
+```shell
+cd pyMetaIO
+conda env create --name pyMetaIO --file environment.yml
+conda activate pyMetaIO
+pytest
+```
---
+++ tests/test_io.py
@@ -0,0 +1,61 @@
+import contextlib
+import pathlib
+import tempfile
+
+import numpy as np
+import pymetaio as mio
+import pytest
+
+DTYPE = np.byte, np.ubyte, np.short, np.ushort, np.int, np.uint, np.single, np.double
+SUFFIX = '.mha', '.mhd'
+
+
+@contextlib.contextmanager
+def file_with_suffix(suffix):
+    tf = tempfile.NamedTemporaryFile(suffix=suffix, delete=False)
+    tf.close()
+    filepath = pathlib.Path(tf.name)
+    try:
+        yield filepath
+    finally:
+        for s in (suffix, '.raw', '.zraw'):
+            fws = filepath.with_suffix(s)
+            if fws.is_file():
+                fws.unlink()
+
+
+@pytest.mark.parametrize('dtype', DTYPE)
+def test_compression(dtype):
+    with file_with_suffix('.mhd') as f:
+        a = (100 * np.random.random_sample((2, 3, 4))).astype(dtype)
+        mio.write_image(f, a, header={'CompressedData': True})
+        b, _ = mio.read_image(f)
+        np.testing.assert_almost_equal(b, a)
+
+
+def test_header():
+    with file_with_suffix('.mhd') as f:
+        a = np.random.random_sample((2, 3, 4))
+        mio.write_image(f, a)
+        b, _ = mio.read_image(f, slices=())
+        assert b is None
+
+
+@pytest.mark.parametrize('suffix', SUFFIX)
+@pytest.mark.parametrize('dtype', DTYPE)
+def test_io(suffix, dtype):
+    with file_with_suffix(suffix) as f:
+        a = (100 * np.random.random_sample((2, 3, 4))).astype(dtype)
+        mio.write_image(f, a)
+        b, _ = mio.read_image(f)
+        np.testing.assert_almost_equal(b, a)
+
+
+@pytest.mark.parametrize('dtype', DTYPE)
+def test_memmap(dtype):
+    with file_with_suffix('.mhd') as f:
+        a = (100 * np.random.random_sample((2, 3, 4))).astype(dtype)
+        mio.write_image(f, a)
+        b, _ = mio.read_image(f, memmap=True)
+        np.testing.assert_almost_equal(b, a)
+        del b
