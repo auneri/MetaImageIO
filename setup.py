54e1e4cc8675333bf71d6221996d46e5dee9ff0b
Ali Uneri <ali.uneri@jhu.edu>
2020-11-01 23:09:33 -0500

    Update dependencies

--- setup.py
+++ setup.py
@@ -29,4 +29,4 @@ setuptools.setup(
         'Programming Language :: Python :: 3'],
     packages=setuptools.find_packages(),
     install_requires=install_requires(),
-    python_requires='>=3.7')
+    python_requires='>=3.6')
