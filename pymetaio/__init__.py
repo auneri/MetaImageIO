db5ec2ab1ea2899208f5fe0a6f18c33a1dfef9ba
Ali Uneri <ali.uneri@jhu.edu>
2020-11-17 00:23:08 -0500

    Define version in a dedicated module

--- pymetaio/__init__.py
+++ pymetaio/__init__.py
@@ -1,6 +1,3 @@
-import os as _os
-
 from . import imageio, util  # noqa: F401
 from .io import read_image, write_image  # noqa: F401
-
-__version__ = f'0.1.{_os.getenv("PYMETAIO_COMMIT_COUNT", 0)}'
+from .version import __version__  # noqa: F401
