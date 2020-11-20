e10948d9aa0eec7d909cc6551fe93ee9cae71651
Ali Uneri <ali.uneri@jhu.edu>
2020-11-20 15:44:54 -0500

    Avoid import errors during version check

--- pymetaio/__init__.py
+++ pymetaio/__init__.py
@@ -1,3 +1,3 @@
-from . import imageio, util  # noqa: F401
-from .io import read_image, write_image  # noqa: F401
 from .version import __version__  # noqa: F401
+from . import imageio, util  # noqa: F401, I100
+from .io import read_image, write_image  # noqa: F401
