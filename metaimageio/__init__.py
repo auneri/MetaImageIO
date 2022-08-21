0fa21247630d90788789f7a2f1c5c078f1134273
Ali Uneri <ali.uneri@jhu.edu>
2022-08-21 15:38:15 -0400

    Use different names for modules and functions

--- metaimageio/__init__.py
+++ metaimageio/__init__.py
@@ -1,5 +1,5 @@
 from .version import __version__  # noqa: F401
 from . import util  # noqa: F401, I100
 from .imageio import imageio  # noqa: F401
-from .read import read  # noqa: F401
-from .write import write  # noqa: F401
+from .reader import read  # noqa: F401
+from .writer import write  # noqa: F401
--- metaimageio/imageio.py
+++ metaimageio/imageio.py
@@ -2,7 +2,7 @@ import numpy as np
 from imageio import formats
 from imageio.core import Format
 
-from . import read, write
+from . import reader, writer
 
 EXTENSIONS = '.mha', '.mhd'
 
@@ -29,7 +29,7 @@ class MetaImageIOFormat(Format):
         def _get_data(self, index, **kwargs):
             if index != 0:
                 raise NotImplementedError('MetaImageIO does not support non-zero indices')
-            image, meta = read.read(self._filepath, **self.request.kwargs)
+            image, meta = reader.read(self._filepath, **self.request.kwargs)
             if image is None:
                 image = np.array(())
             return image, meta
@@ -37,7 +37,7 @@ class MetaImageIOFormat(Format):
         def _get_meta_data(self, index):
             if index != 0:
                 raise NotImplementedError('MetaImageIO does not support non-zero indices')
-            _, meta = read.read(self._filepath, slices=())
+            _, meta = reader.read(self._filepath, slices=())
             return meta
 
     class Writer(Format.Writer):
@@ -51,7 +51,7 @@ class MetaImageIOFormat(Format):
         def _append_data(self, im, meta):
             meta.pop('ElementDataFile', None)
             meta.update(self.request.kwargs)
-            write.write(self._filepath, image=im, **meta)
+            writer.write(self._filepath, image=im, **meta)
 
         def set_meta_data(self, meta):
             raise NotImplementedError('MetaImageIO does not support writing meta data')
